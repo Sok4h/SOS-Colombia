d32d375ac48fd61b116570bfc4fadf1637d642e6
Juan David Bravo <juanda.bravo98@gmail.com>
2021-05-05 18:13:11 -0500

    Created donation card script

---
+++ js/utils/createCard.js
@@ -0,0 +1,56 @@
+const createCard = (type, data) => {
+    const card = document.createElement('article');
+    card.classList.add('card');
+
+    const header = document.createElement('header');
+    card.appendChild(header);
+    header.classList.add('card__header');
+
+    const titleWrapper = document.createElement('div');
+    header.appendChild(titleWrapper);
+
+    const title = document.createElement('h3');
+    title.classList.add('card__title');
+    titleWrapper.appendChild(title);
+    title.innerText = data.title;
+
+    if(data.subtitle) {
+        const subtitle = document.createElement('p');
+        subtitle.classList.add('card__subtitle');
+        titleWrapper.appendChild(subtitle);
+        subtitle.innerText = data.subtitle;
+    }
+
+    if(data.section) {
+        const sectionText = document.createElement('p');
+        sectionText.classList.add('card__section');
+        sectionText.innerText = data.section;
+        card.appendChild(sectionText);
+    }
+
+    if(data.list) {
+        const list = document.createElement('ul');
+        list.classList.add('card__list');
+        card.appendChild(list);
+        data.list.forEach(element => {
+            const item = document.createElement('li');
+            item.classList.add('card__listItem');
+            list.appendChild(item);
+            try {
+                const json = JSON.parse(element);
+                item.innerHTML = `<b> ${json.name} </b> <br> ${json.number}`;
+            } catch {
+                item.innerText = element;
+                throw new Error(element);
+            }
+        });
+    }
+
+    if(type == 'dark') {
+        card.classList.add('card--dark');
+    } else if(type == 'white') {
+        card.classList.add('card--white');
+    }
+
+    return card;
+}
