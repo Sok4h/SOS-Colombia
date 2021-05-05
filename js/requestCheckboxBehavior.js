d995870be0a43cfd6912da0dcd7c1bceb9dd2655
Juan David Payán <JuanDavidPF@gmail.com>
2021-05-05 18:18:02 -0500

    Fixed section checbkox behaviour

--- js/requestCheckboxBehavior.js
+++ js/requestCheckboxBehavior.js
@@ -1,56 +1,124 @@
-window.addEventListener('load', () => {
-    const basicToggle = document.querySelector('input[name="basic"]');
-    const basicExpand = document.querySelector('input[name="basicTitle"]');
-    const basicCheckboxes = document.querySelectorAll('input[name="basicRequest"]');
+window.addEventListener("load", () => {
+  const basicToggle = document.querySelector('input[name="basic"]');
+  const basicExpand = document.querySelector('input[name="basicTitle"]');
+  const basicCheckboxes = document.querySelectorAll(
+    'input[name="basicRequest"]'
+  );
 
-    const equipmentToggle = document.querySelector('input[name="equipment"]');
-    const equipmentExpand = document.querySelector('input[name="equipmentTitle"]');
-    const equipmentCheckboxes = document.querySelectorAll('input[name="equipmentRequest"]');
+  const equipmentToggle = document.querySelector('input[name="equipment"]');
+  const equipmentExpand = document.querySelector(
+    'input[name="equipmentTitle"]'
+  );
+  const equipmentCheckboxes = document.querySelectorAll(
+    'input[name="equipmentRequest"]'
+  );
 
-    const injectologyToggle = document.querySelector('input[name="injectology"]');
-    const injectologyExpand = document.querySelector('input[name="injectologyTitle"]');
-    const injectologyCheckboxes = document.querySelectorAll('input[name="injectologyRequest"]');
+  const injectologyToggle = document.querySelector('input[name="injectology"]');
+  const injectologyExpand = document.querySelector(
+    'input[name="injectologyTitle"]'
+  );
+  const injectologyCheckboxes = document.querySelectorAll(
+    'input[name="injectologyRequest"]'
+  );
 
-    const medicineToggle = document.querySelector('input[name="medicine"]');
-    const medicineExpand = document.querySelector('input[name="medicineTitle"]');
-    const medicineCheckboxes = document.querySelectorAll('input[name="medicineRequest"]');
+  const medicineToggle = document.querySelector('input[name="medicine"]');
+  const medicineExpand = document.querySelector('input[name="medicineTitle"]');
+  const medicineCheckboxes = document.querySelectorAll(
+    'input[name="medicineRequest"]'
+  );
 
-    const solutionsToggle = document.querySelector('input[name="solutions"]');
-    const solutionsExpand = document.querySelector('input[name="solutionsTitle"]');
-    const solutionsCheckboxes = document.querySelectorAll('input[name="solutionsRequest"]');
+  const solutionsToggle = document.querySelector('input[name="solutions"]');
+  const solutionsExpand = document.querySelector(
+    'input[name="solutionsTitle"]'
+  );
+  const solutionsCheckboxes = document.querySelectorAll(
+    'input[name="solutionsRequest"]'
+  );
 
-    const woundsToggle = document.querySelector('input[name="wounds"]');
-    const woundsExpand = document.querySelector('input[name="woundsTitle"]');
-    const woundsCheckboxes = document.querySelectorAll('input[name="woundsRequest"]');
+  const woundsToggle = document.querySelector('input[name="wounds"]');
+  const woundsExpand = document.querySelector('input[name="woundsTitle"]');
+  const woundsCheckboxes = document.querySelectorAll(
+    'input[name="woundsRequest"]'
+  );
 
-    basicToggle.addEventListener('change', (event) => {
-        toggleSection(basicToggle, basicExpand, basicCheckboxes);
+  attachListenersToCheckboxes();
+
+  basicToggle.addEventListener("change", (event) => {
+    toggleSection(basicToggle, basicExpand, basicCheckboxes);
+  });
+
+  equipmentToggle.addEventListener("change", (event) => {
+    toggleSection(equipmentToggle, equipmentExpand, equipmentCheckboxes);
+  });
+
+  injectologyToggle.addEventListener("change", (event) => {
+    toggleSection(injectologyToggle, injectologyExpand, injectologyCheckboxes);
+  });
+
+  medicineToggle.addEventListener("change", (event) => {
+    toggleSection(medicineToggle, medicineExpand, medicineCheckboxes);
+  });
+
+  solutionsToggle.addEventListener("change", (event) => {
+    toggleSection(solutionsToggle, solutionsExpand, solutionsCheckboxes);
+  });
+
+  woundsToggle.addEventListener("change", (event) => {
+    toggleSection(woundsToggle, woundsExpand, woundsCheckboxes);
+  });
+
+  function toggleSection(toggle, expand, checkboxes) {
+    expand.checked = true;
+    checkboxes.forEach((checkbox) => {
+      checkbox.checked = toggle.checked;
+    });
+  }
+
+  function verifySection(sectionToggle, sectionCheckboxes) {
+    let allChecked = true;
+
+    sectionCheckboxes.forEach((checkbox) => {
+      if (!checkbox.checked) allChecked = false;
     });
 
-    equipmentToggle.addEventListener('change', (event) => {
-        toggleSection(equipmentToggle, equipmentExpand, equipmentCheckboxes);
+    sectionToggle.checked = allChecked;
+  } //closes toggleCheckbox method
+
+  function attachListenersToCheckboxes() {
+    basicCheckboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
+        verifySection(basicToggle, basicCheckboxes);
+      });
     });
 
-    injectologyToggle.addEventListener('change', (event) => {
-        toggleSection(injectologyToggle, injectologyExpand, injectologyCheckboxes);
+    equipmentCheckboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
+        verifySection(equipmentToggle, equipmentCheckboxes);
+      });
     });
 
-    medicineToggle.addEventListener('change', (event) => {
-        toggleSection(medicineToggle, medicineExpand, medicineCheckboxes);
+    injectologyCheckboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
+        verifySection(injectologyToggle, injectologyCheckboxes);
+      });
     });
 
-    solutionsToggle.addEventListener('change', (event) => {
-        toggleSection(solutionsToggle, solutionsExpand, solutionsCheckboxes);
+    medicineCheckboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
+        verifySection(medicineToggle, medicineCheckboxes);
+      });
     });
 
-    woundsToggle.addEventListener('change', (event) => {
-        toggleSection(woundsToggle, woundsExpand, woundsCheckboxes);
+    solutionsCheckboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
+        verifySection(solutionsToggle, solutionsCheckboxes);
+      });
     });
 
-    function toggleSection(toggle, expand, checkboxes) {
-        expand.checked = true;
-        checkboxes.forEach(checkbox => {
-            checkbox.checked = toggle.checked;
-        });
-    }
+    woundsCheckboxes.forEach((checkbox) => {
+      checkbox.addEventListener("change", () => {
+        verifySection(woundsToggle, woundsCheckboxes);
+      });
+    });
+  } //closes attachListeners function
 });
