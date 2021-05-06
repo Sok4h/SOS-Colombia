707442356aec6752898459a63a3b58b458f525d9
Juan David Payán <JuanDavidPF@gmail.com>
2021-05-06 03:31:19 -0500

    Finished location selection input

--- js/firebase/locationSelection.js
+++ js/firebase/locationSelection.js
@@ -7,9 +7,8 @@ const locations = {
   Oriente: [],
   Sur: [],
   Oeste: [],
-  Centro:[]
+  Centro: [],
 };
-
 
 const LoadLocation = () => {
   db.collection("supplies_concentration_spots")
@@ -21,21 +20,65 @@ const LoadLocation = () => {
           name: locationDB.data().name,
           phones: locationDB.data().phone,
           zone: locationDB.data().sector,
+          id: locationDB.id,
         };
 
         locations[location.zone].push(location);
-        var opt = document.createElement("option");
-        opt.value = locationDB.id;
-        opt.innerHTML = location.name;
-        AddOptionToInputs(opt);
       });
- 
+      AddOptionToInputs();
     });
 };
 
-const AddOptionToInputs = (option) => {
+const AddOptionToInputs = () => {
   selectLocationInputs.forEach((locationInput) => {
-    locationInput.appendChild(option);
+    var northOtGroup = document.createElement("optgroup");
+    northOtGroup.label = "Norte";
+    locationInput.appendChild(northOtGroup);
+
+    locations.Norte.forEach((location) => {
+      var opt = document.createElement("option");
+      opt.innerHTML = location.name;
+      opt.value = location.id;
+      northOtGroup.appendChild(opt);
+    });
+
+    var eastOtGroup = document.createElement("optgroup");
+    eastOtGroup.label = "Oriente";
+    locationInput.appendChild(eastOtGroup);
+
+    locations.Oriente.forEach((location) => {
+      var opt = document.createElement("option");
+      opt.innerHTML = location.name;
+      opt.value = location.id;
+      eastOtGroup.appendChild(opt);
+    });
+
+
+    var southGroup = document.createElement("optgroup");
+    southGroup.label = "South";
+    locationInput.appendChild(southGroup);
+
+    locations.Sur.forEach((location) => {
+      var opt = document.createElement("option");
+      opt.innerHTML = location.name;
+      opt.value = location.id;
+      southGroup.appendChild(opt);
+    });
+
+    
+    var westGroup = document.createElement("optgroup");
+    westGroup.label = "Oeste";
+    locationInput.appendChild(westGroup);
+    
+    locations.Oeste.forEach((location) => {
+      var opt = document.createElement("option");
+      opt.innerHTML = location.name;
+      opt.value = location.id;
+      westGroup.appendChild(opt);
+    });
+
+
+
   });
 };
 
