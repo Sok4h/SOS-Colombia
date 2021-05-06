0598f981094a444b4b443443d486c1e1ab7c1180
Juan David Payán <JuanDavidPF@gmail.com>
2021-05-06 04:50:04 -0500

    Finished Get Supply Reequest

--- js/firebase/getSuppliesGatheringInfo.js
+++ js/firebase/getSuppliesGatheringInfo.js
@@ -1,35 +1,39 @@
-window.addEventListener('load', () => {
-    const suppliesEastContainer = document.getElementById('suppliesInfo-east');
-    const suppliesNorthContainer = document.getElementById('suppliesInfo-north');
-    const suppliesCenterContainer = document.getElementById('suppliesInfo-center');
-    const suppliesSouthContainer = document.getElementById('suppliesInfo-south');
-    const suppliesWestContainer = document.getElementById('suppliesInfo-west');
+window.addEventListener("load", () => {
+  const suppliesEastContainer = document.getElementById("suppliesInfo-east");
+  const suppliesNorthContainer = document.getElementById("suppliesInfo-north");
+  const suppliesCenterContainer = document.getElementById(
+    "suppliesInfo-center"
+  );
+  const suppliesSouthContainer = document.getElementById("suppliesInfo-south");
+  const suppliesWestContainer = document.getElementById("suppliesInfo-west");
 
-    loadSuppliesInfo(suppliesEastContainer, 'Oriente');
-    loadSuppliesInfo(suppliesNorthContainer, 'Norte');
-    loadSuppliesInfo(suppliesCenterContainer, 'Centro');
-    loadSuppliesInfo(suppliesSouthContainer, 'Sur')
-    loadSuppliesInfo(suppliesWestContainer, 'Occidente');
-    
+  loadSuppliesInfo(suppliesEastContainer, "Oriente");
+  loadSuppliesInfo(suppliesNorthContainer, "Norte");
+  loadSuppliesInfo(suppliesCenterContainer, "Centro");
+  loadSuppliesInfo(suppliesSouthContainer, "Sur");
+  loadSuppliesInfo(suppliesWestContainer, "Occidente");
 
-    function loadSuppliesInfo(container,query) {
-        db.collection('supplies_gathering_info').where('sector', '==', query).get()
-        .then(data => {
-            let height = 0;
-            data.forEach(supplieGathering => {
-                const supplieGatheringData = {
-                    title: supplieGathering.data().name,
-                    subtitle: supplieGathering.data().address,
-                    section: 'Teléfono',
-                    list: supplieGathering.data().phone
-                }
-    
-                height++;
-                const card = createCard('dark', supplieGatheringData, 'li');
-                container.appendChild(card);
-            });
-    
-            container.style.setProperty('--height', `${height*20}rem`);
-        })
-    }
+  function loadSuppliesInfo(container, query) {
+    db.collection("supplies_gathering_info")
+      .where("sector", "==", query)
+      .get()
+      .then((data) => {
+        let height = 0;
+        data.forEach((supplieGathering) => {
+          const supplieGatheringData = {
+            title: supplieGathering.data().name,
+            subtitle: supplieGathering.data().address,
+            section: "Teléfono",
+            list: supplieGathering.data().phone,
+          };
+
+      
+          height++;
+          const card = createCard("dark", supplieGatheringData, "li");
+          container.appendChild(card);
+        });
+
+        container.style.setProperty("--height", `${height * 20}rem`);
+      });
+  }
 });
---
+++ js/firebase/getSupplyRequests.js
@@ -0,0 +1,64 @@
+const NorthRequestContainer = document.querySelector(".help-section__North");
+const SouthRequestContainer = document.querySelector(".help-section__South");
+const CenterRequestContainer = document.querySelector(".help-section__Center");
+const WestRequestContainer = document.querySelector(".help-section__West");
+const EastRequestContainer = document.querySelector(".help-section__East");
+
+loadRequestInfo(NorthRequestContainer, "Norte");
+loadRequestInfo(SouthRequestContainer, "Sur");
+loadRequestInfo(CenterRequestContainer, "Center");
+loadRequestInfo(WestRequestContainer, "Oeste");
+loadRequestInfo(EastRequestContainer, "Oriente");
+
+function loadRequestInfo(container, query) {
+  db.collection("requests")
+    .where("sector", "==", query)
+    .get()
+    .then((data) => {
+      let height = 1;
+      data.forEach((requestData) => {
+        const request = {
+          author: requestData.data().author,
+          zone: requestData.data().sector,
+          location: requestData.data().supply_concentration_spot,
+          time: requestData.data().timestamp,
+          supplies: requestData.data().supplies,
+          id: requestData.id,
+        };
+
+        const suppliesNames = [];
+        request.supplies.forEach((supply) => {
+          let supplyName = JSON.parse(supply);
+
+          suppliesNames.push(supplyName.name);
+        });
+     
+
+        db.collection("supplies_concentration_spots")
+          .doc(request.location)
+          .get()
+          .then((locationData) => {
+            const location = {
+              address: locationData.data().address,
+              name: locationData.data().name,
+              phones: locationData.data().phone,
+              zone: locationData.data().sector,
+              id: locationData.id,
+            };
+
+            const cardData = {
+              title: location.name,
+              subtitle: location.address,
+              section: "Aqui iria la descripción del request",
+              list: suppliesNames,
+            };
+
+            height++;
+            const card = createCard("", cardData, "li");
+            container.appendChild(card);
+          });
+      });
+
+      container.style.setProperty("--height", `${height * 100}rem`);
+    });
+}
