5dad7cf9c9e8b537db2b40bd5d185c05491c3538
Juan David Bravo <juanda.bravo98@gmail.com>
2021-05-05 21:49:10 -0500

    Added script to load supplies info from db

--- js/firebase/getSuppliesGatheringInfo.js
+++ js/firebase/getSuppliesGatheringInfo.js
@@ -1,18 +1,35 @@
 window.addEventListener('load', () => {
     const suppliesEastContainer = document.getElementById('suppliesInfo-east');
+    const suppliesNorthContainer = document.getElementById('suppliesInfo-north');
+    const suppliesCenterContainer = document.getElementById('suppliesInfo-center');
+    const suppliesSouthContainer = document.getElementById('suppliesInfo-south');
+    const suppliesWestContainer = document.getElementById('suppliesInfo-west');
 
-    db.collection('supplies_gathering_info').get()
-    .then(suppliesGathering => {
-        suppliesGathering.forEach(supplieGathering => {
-            console.log(supplieGathering.data().name);
-            const supplieGatheringData = {
-                title: supplieGathering.data().name,
-                subtitle: supplieGathering.data().address,
-                //list: supplieGathering.data().phone
-            }
+    loadSuppliesInfo(suppliesEastContainer, 'Oriente');
+    loadSuppliesInfo(suppliesNorthContainer, 'Norte');
+    loadSuppliesInfo(suppliesCenterContainer, 'Centro');
+    loadSuppliesInfo(suppliesSouthContainer, 'Sur')
+    loadSuppliesInfo(suppliesWestContainer, 'Occidente');
+    
 
-            const card = createCard('dark', supplieGatheringData);
-            suppliesEastContainer.appendChild(card);
-        });
-    })
+    function loadSuppliesInfo(container,query) {
+        db.collection('supplies_gathering_info').where('sector', '==', query).get()
+        .then(data => {
+            let height = 0;
+            data.forEach(supplieGathering => {
+                const supplieGatheringData = {
+                    title: supplieGathering.data().name,
+                    subtitle: supplieGathering.data().address,
+                    section: 'Teléfono',
+                    list: supplieGathering.data().phone
+                }
+    
+                height++;
+                const card = createCard('dark', supplieGatheringData, 'li');
+                container.appendChild(card);
+            });
+    
+            container.style.setProperty('--height', `${height*20}rem`);
+        })
+    }
 });
