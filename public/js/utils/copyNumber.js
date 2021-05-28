9d03228d0ffd517cba09dc1ffa38750c27b987ea
Jhon Garcia <metalprime57@gmail.com>
2021-05-28 15:32:37 -0500

    copy number

---
+++ public/js/utils/copyNumber.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", () =>{
+
+    const copyNumberBtn = document.getElementById("create-donation-btn");
+
+    copyNumberBtn.addEventListener("click", () =>{
+        /* Get the text field */
+        var copyText = document.getElementById("number-copy");
+      
+        /* Select the text field */
+        copyText.select();
+        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+      
+        /* Copy the text inside the text field */
+        document.execCommand("copy");
+      
+        /* Alert the copied text */
+        alert("Texto copiado");
+    });
+
+   
+
+      
+})
+
