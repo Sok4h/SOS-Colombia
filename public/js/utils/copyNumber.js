3649e6743a0f9f39e42d3c8b0638e0215cc824af
Jhon Garcia <metalprime57@gmail.com>
2021-05-28 15:43:02 -0500

    update in btn copy

--- public/js/utils/copyNumber.js
+++ public/js/utils/copyNumber.js
@@ -1,14 +1,14 @@
 window.addEventListener("load", () =>{
 
-    const copyNumberBtn = document.getElementById("create-donation-btn");
+    const copyNumberBtn = document.getElementById("copy-number");
 
     copyNumberBtn.addEventListener("click", () =>{
         /* Get the text field */
         var copyText = document.getElementById("number-copy");
       
         /* Select the text field */
-        copyText.select();
-        copyText.setSelectionRange(0, 99999); /* For mobile devices */
+        copyText.innerHTML;
+         /* For mobile devices */
       
         /* Copy the text inside the text field */
         document.execCommand("copy");
